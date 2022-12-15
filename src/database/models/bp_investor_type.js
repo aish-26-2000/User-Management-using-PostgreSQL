0710e3f6eba3d4939be6e3dd3a2d81642fa63a6a
aish-26-2000 <sreedeviaishu41@gmail.com>
2022-12-15 15:50:54 +0530

    master tables added

---
+++ src/database/models/bp_investor_type.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class bp_investor_type extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        // static associate(models) {
+        // define association here
+        // }
+    }
+    bp_investor_type.init(
+        {
+            bp_investor_type_id: DataTypes.UUID,
+            isactive: DataTypes.STRING,
+            name: DataTypes.STRING,
+            description: DataTypes.STRING,
+            created: DataTypes.DATE,
+            createdby: DataTypes.STRING,
+            updated: DataTypes.DATE,
+            updatedby: DataTypes.STRING,
+            shortcode: DataTypes.STRING,
+            sequenceno: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: bp_investor_type.name,
+            tableName: 'bp_investor_type',
+            timestamps: false,
+        }
+    );
+    return bp_investor_type;
+};
---
+++ src/database/models/bp_user_association.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class bp_user_association extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        // static associate(models) {
+        // define association here
+        // }
+    }
+    bp_user_association.init(
+        {
+            bp_user_association_id: DataTypes.UUID,
+            isactive: DataTypes.STRING,
+            name: DataTypes.STRING,
+            description: DataTypes.STRING,
+            created: DataTypes.DATE,
+            createdby: DataTypes.STRING,
+            updated: DataTypes.DATE,
+            updatedby: DataTypes.STRING,
+            shortcode: DataTypes.STRING,
+            sequenceno: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: bp_user_association.name,
+            tableName: 'bp_user_association',
+            timestamps: false,
+        }
+    );
+    return bp_user_association;
+};
